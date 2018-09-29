6a078d602c9e18b28c3ca381a2c7c8fbab1bbc59
Rodrigo Lessa <rodrigolsr@gmail.com>
2018-09-29 16:28:40 -0300

    histogram tests

--- tests/histogram_test.py
+++ tests/histogram_test.py
@@ -2,7 +2,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('pedestre.tif', 0)
+img = cv2.imread('pedestrian_test.png', 0)
 
 hist, bins = np.histogram(img.flatten(), 256, [0, 256])
 
