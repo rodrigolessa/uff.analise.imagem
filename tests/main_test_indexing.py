013edc44d58db18e65eb91d5c27c6c3685c3909f
Rodrigo Lessa <rodrigolsr@gmail.com>
2018-10-15 00:26:33 -0300

    sequencia

--- tests/main_test_indexing.py
+++ tests/main_test_indexing.py
@@ -13,6 +13,8 @@ import numpy as np
 import cv2
 import pickle as cp
 import glob
+import os
+import sys
 
 class MainIndexing:
 
@@ -21,6 +23,10 @@ class MainIndexing:
         self.imageMomentsFile = 'index.pkl'
         self.index = {}
         # Initialize descriptor with a radius of 21 pixels
+        # radius : integer
+        # the maximum radius for the Zernike polynomials, in pixels. 
+        # Note that the area outside the circle (centered on center of mass) 
+        # defined by this radius is ignored.
         self.zm = ZernikeMoments(21)
 
         # If index file exists, try to delete
@@ -39,11 +45,20 @@ class MainIndexing:
         # Pad the image with extra white pixels to ensure the
         # edges of the object are not up against the borders
         # of the image
-        #image = cv2.copyMakeBorder(image, 15, 15, 15, 15, cv2.BORDER_CONSTANT, value = 255)
+        #img = cv2.copyMakeBorder(img, 15, 15, 15, 15, cv2.BORDER_CONSTANT, value = 255)
+        
+        img = cv2.resize(img, None, fx=0.75, fy=0.75, interpolation = cv2.INTER_CUBIC)
 
         # Debugging: Show Original
         cv2.imshow('original', img)
         cv2.waitKey(0)
+
+        bordersize = 100
+
+        #bordered = cv2.copyMakeBorder(img, 
+        #    top=bordersize, bottom=bordersize, left=bordersize, right=bordersize, 
+        #    borderType= cv2.BORDER_CONSTANT, 
+        #    value=[255,255,255])
 
         ######################################################
         # Espaços de cores da imagem
@@ -59,26 +74,82 @@ class MainIndexing:
         cv2.waitKey(0)
 
         ######################################################
-        # Filtros para remover ruídos
+        # Verificar se a imagem possui baixo constraste
+
+        # The low contrast fraction threshold. An image is considered low-contrast 
+        # when its range of brightness spans less than this fraction of its data type’s 
+        # full range. [1]
+
+        #skimage.exposure.is_low_contrast(image, fraction_threshold=0.05, lower_percentile=1, upper_percentile=99, method='linear')
+
+        ######################################################
+        # Equalização baseado em histograma da imagem
+
+        #CLAHE (Contrast Limited Adaptive Histogram Equalization)
+
+        # create a CLAHE object (Arguments are optional).
+        #clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+        clahe = cv2.createCLAHE()
+
+        cl1 = clahe.apply(grayscale)
+
+        #res = np.hstack((img, cl1))
+        #cv2.imwrite('res.png', res)
+        cv2.imshow("Equalization", cl1)
+        cv2.waitKey(0)
+
+        ######################################################
+        # Algoritmos de redução de ruido:
 
         # Apply a blur filter to reduce noise
-		blur = cv2.medianBlur(grayscale, 5)
+        #blur = cv2.medianBlur(cl1, 5)
 
-		# Bilateral Filter can reduce unwanted noise
-		blur = cv2.bilateralFilter(grayscale, 9, 75, 75)
+        #suave = cv2.GaussianBlur(grayscale, (11, 11), 0)
+
+        # Bilateral Filter can reduce unwanted noise
+        #blur = cv2.bilateralFilter(cl1, 9, 75, 75)
+        blur = cv2.bilateralFilter(cl1, 7, 49, 49)
+        
+        # "fastNlMeansDenoising"
+        
+        #row, col = cl1.shape
+        #mean = 0
+
+        #gauss = np.random.normal(mean, 1, (row, col))
+        #gauss = gauss.reshape(row, col)
+        
+        #noisy = cl1 + gauss
+        #noisy = (noisy).astype('uint8')
+
+        #cv2.imshow('Blur', noisy)
+        #cv2.waitKey(0)
+
+        #for i in range(3):
+
+            #noisy = cv2.fastNlMeansDenoising(noisy, templateWindowSize=5, searchWindowSize=25, h=65)
+            #cv2.imshow('Blur', noisy)
+            #cv2.waitKey(0)
+            #noisy = cv2.threshold(noisy, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+            #_, noisy = cv2.threshold(noisy, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+        # self.view(img, noisy)
+        #cv2.imshow('Blur', noisy)
 
         #cv2.imwrite(os.path.join(debugPath , '{}_blur.png'.format(imageName)), blur)
 
+        cv2.imshow('Blur', blur)
+        cv2.waitKey(0)
+
         ######################################################
         # Inverter intensidades
-    
+
         # For segmentation: Flip the values of the pixels 
         # (black pixels are turned to white, and white pixels to black).
-        inv = cv2.bitwise_not(blur)
+        #inv = cv2.bitwise_not(blur)
 
         # Debugging: Invert image
-        cv2.imshow('inverted', inv)
-        cv2.waitKey(0)
+        #cv2.imshow('Inverted', inv)
+        #cv2.waitKey(0)
 
         ######################################################
         # Limiar na imagem
@@ -87,17 +158,45 @@ class MainIndexing:
         #inv[inv > 0] = 255
         #thresh = inv
 
-		# Then, any pixel with a value greater than zero (black) is set to 255 (white)
-		# NOTE: First version
-		#thresh[thresh > 0] = 255
-		# NOTE: Second version
-		# THRESH_BINARY = fundo preto or THRESH_BINARY_INV = fundo branco
-		# Then, any pixel with a value greater than zero (black) is set to 255 (white)
-		_, thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+
+        # Canny edge detector finds edge like regions in the image
+        #edged = cv2.Canny(inv, 30, 200)
+
+
+        # Then, any pixel with a value greater than zero (black) is set to 255 (white)
+        # NOTE: First version
+        #thresh[thresh > 0] = 255
+        # NOTE: Second version
+        # THRESH_BINARY = fundo preto or THRESH_BINARY_INV = fundo branco
+        # Then, any pixel with a value greater than zero (black) is set to 255 (white)
+        _, thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
         # Debugging: Threshold it
         cv2.imshow('thresholded', thresh)
         cv2.waitKey(0)
+
+        ######################################################
+        # Detectar contornos na imagem
+
+        # Initialize the outline image,
+        # find the outermost contours (the outline) of the object, 
+        # cv2.RETR_EXTERNAL - telling OpenCV to find only the outermost contours.
+        # cv2.CHAIN_APPROX_SIMPLE - to compress and approximate the contours to save memory
+        img2, contours, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        # Sort the contours based on their area, in descending order. 
+        # keep only the largest contour and discard the others.
+        #contours = sorted(contours, key = cv2.contourArea, reverse = True)[0]
+        contours = sorted(contours, key = cv2.contourArea, reverse = True)
+
+        print('Total of contours')
+        print(len(contours))
+
+        # Return only the 10 largest contours
+        #cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
+
+        ######################################################
+        # Reescrevendo os contornos nas imagens
 
         # First, we need a blank image to store outlines
         # we appropriately a variable called outline 
@@ -108,20 +207,13 @@ class MainIndexing:
         # type of image data, number of pixels etc.
         # Shape of image is accessed by img.shape. It returns a tuple of number of rows, 
         # columns and channels (if image is color):
-        outline = np.zeros(image.shape, dtype = "uint8")
-
-        # Initialize the outline image,
-        # find the outermost contours (the outline) of the object, 
-        # cv2.RETR_EXTERNAL - telling OpenCV to find only the outermost contours.
-        # cv2.CHAIN_APPROX_SIMPLE - to compress and approximate the contours to save memory
-        img2, contours, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-        # Sort the contours based on their area, in descending order. 
-        # keep only the largest contour and discard the others.
-        contours = sorted(contours, key = cv2.contourArea, reverse = True)[0]
+        outline = np.zeros(img.shape, dtype = "uint8")
 
         # The outline is drawn as a filled in mask with white pixels:
-        cv2.drawContours(outline, [contours], -1, 255, -1)
+        cv2.drawContours(outline, contours[:10], -1, 255, -1)
+
+        # Drawing our screen contours, we can clearly see that we have found the Object screen
+        #cv2.drawContours(outline, [contours], -1, (0, 255, 0), 3)
 
         # Debugging: just outline of the object
         cv2.imshow('outline', outline)
@@ -129,7 +221,7 @@ class MainIndexing:
         cv2.destroyAllWindows()
 
         # Compute Zernike moments to characterize the shape of object outline
-        moments = self.zm.describe(outline)
+        #moments = self.zm.describe(outline)
 
         # then update the index
         #self.index[imageName] = moments
--- tests/zernike_moments.py
+++ tests/zernike_moments.py
@@ -11,4 +11,18 @@ class ZernikeMoments:
 		# Return the Zernike moments for the image
 		# http://mahotas.readthedocs.io/en/latest/api.html#mahotas.features.zernike_moments
 		# mahotas.features.zernike_moments(im, radius, degree=8, cm={center_of_mass(im)})
+		# Parameters:
+		# 	im : 2-ndarray
+		# 		input image
+		# 	radius : integer
+		# 		the maximum radius for the Zernike polynomials, in pixels. 
+		# 		Note that the area outside the circle (centered on center of mass) 
+		# 		defined by this radius is ignored.
+		# 	degree : integer, optional
+		# 		Maximum degree to use (default: 8)
+		# 	cm : pair of floats, optional
+		# 		the centre of mass to use. By default, 
+		# 		uses the image’s centre of mass.
+		# Returns:
+		# 	zvalues : 1-ndarray of floats
 		return mh.features.zernike_moments(image, self.radius)
