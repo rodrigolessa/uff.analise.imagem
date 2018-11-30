58d6ae5c4e026cd5aec113400d700c215838168d
Rodrigo Lessa <rodrigolsr@gmail.com>
2018-11-30 00:54:19 -0200

    test sign

--- tests_traffic_sign/find_triangle.py
+++ tests_traffic_sign/find_triangle.py
@@ -3,7 +3,7 @@
 # iIport the necessary packages
 # The image_utils contains convenience methods to handle basic image processing techniques
 # resizing, rotating, and translating. 
-#import image_utils as iutils
+import imutils
 import numpy as np
 #import argparse
 import cv2
@@ -23,36 +23,49 @@ import cv2
 #args = vars(ap.parse_args())
 
 # Triangle signs:
-#imageName = "dataset_traffic_sign/"400px-Give_way_outdoor.jpg"
-imageName = "dataset_traffic_sign/41-kDNLiI4L._SX425_.jpg"
-#imageName = "dataset_traffic_sign/"141441.jpg"
-#imageName = "dataset_traffic_sign/RW_000005-2.jpg"
+#imageName = "dataset_traffic_sign/400px-Give_way_outdoor.jpg"
+#imageName = "dataset_traffic_sign/41-kDNLiI4L._SX425_.jpg"
+#imageName = "dataset_traffic_sign/141441.jpg"
+imageName = "dataset_traffic_sign/RW_000005-2.jpg"
+imageNumber = "4"
+imageRef = "traffic_sign_canny_douglaspeucker_"
 
 # Load the query image, 
 image = cv2.imread(imageName)
+
 # Only for the test image we have
-# image = iutils.rotate(image, 90, center = None, scale = 1.0)
+# image = imutils.rotate(image, 90, center = None, scale = 1.0)
+
 # compute the ratio of the old height
 # to the new height, clone it, and resize it
 ratio = image.shape[0] / 200.0
-# clone it
-orig = image.copy()
+
 # resize it - The smaller the image is, the faster it is to process
-######image = imutil.resize(image, height = 200)
- 
+image = imutils.resize(image, height = 300)
+
+# clone it
+#original = image.copy()
+cv2.imwrite('traffic_sign_canny_douglaspeucker_' + imageNumber + '1.jpg', image)
+
+print(imageRef + imageNumber + "3.jpg")
+
 # Convert the image to grayscale, blur it, 
 # and find edges in the image
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
 # Blur the image slightly by using the cv2.bilateralFilter function
 # Bilateral filtering has the nice property of removing noise in the image 
 # while still preserving the actual edges.
 gray = cv2.bilateralFilter(gray, 11, 17, 17)
+
 # Canny edge detector finds edge like regions in the image
 edged = cv2.Canny(gray, 30, 200)
 
 # Debugging:
 cv2.imshow('Canny', edged)
 cv2.waitKey(0)
+
+cv2.imwrite('traffic_sign_canny_douglaspeucker_' + imageNumber + '2.jpg', edged)
 
 # Find contours in the edged image, keep only the largest ones, 
 # and initialize our screen contour:
@@ -64,29 +77,47 @@ cv2.waitKey(0)
 image2, cnts, hierarchy = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 # Return only the 10 largest contours
 cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
+
 # Initialize screenCnt, the contour that corresponds to our object to find
 screenCnt = None
 
 # Loop over contours
 for c in cnts:
+
+	#print(c)
+
     # cv2.arcLength and cv2.approxPolyDP. 
     # These methods are used to approximate the polygonal curves of a contour.
 	peri = cv2.arcLength(c, True)
+
+	print(peri)
+
     # Level of approximation precision. 
     # In this case, we use 2% of the perimeter of the contour.
 	approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+
+	print(approx)
 
     # we know that a Object screen is a rectangle,
     # and we know that a rectangle has four sides, thus has four vertices.
 	# If our approximated contour has four points, then
 	# we can assume that we have found our screen.
-	if len(approx) == 4:
+	if len(approx) == 3:
 		screenCnt = approx
 		break
 
 # Drawing our screen contours, we can clearly see that we have found the Object screen
-cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 3)
+#if isinstance(screenCnt, list):
+cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 5)
 cv2.imshow("Object Screen", image)
 cv2.waitKey(0)
+
+cv2.imwrite('traffic_sign_canny_douglaspeucker_' + imageNumber + '3.jpg', image)
+
+#res = np.hstack((original, edged, image))
+
+#cv2.imwrite('traffic_sign_canny_douglaspeucker_1.jpg', res)
+#cv2.imshow("Resultados", res)
+#cv2.waitKey(0)
 
 cv2.destroyAllWindows()
